6d7b4aca4eb39b86e2a18d11cad8c9a0482a01fe
Peter Ungvari <ungvari.mail@gmail.com>
2013-08-22 18:04:11 +0200

    This is the beginning of a beautiful coding style. :)

--- octojs-view.js
+++ octojs-view.js
@@ -1,8 +1,16 @@
 define(["jquery", "octojs-clock"], function($, octoclock) {
 	
-	/** Time arcs view of the clock. */
-	var TimeArcsView = function(canvas) {
-		
+	/** 
+	 * Time arcs view of the clock.
+	 * This is a constructor function.
+	 *
+	 * Public method: update(octalTime)
+	 * 
+	 */
+	var TimeArcsView = function() {
+		/**
+		 * Arcs as clockhand visualizations with initial data.
+		 */
 		var arcs = {
 			outer: { 
 				radius: 60,
@@ -18,13 +26,37 @@ define(["jquery", "octojs-clock"], function($, octoclock) {
 			}
 		};
 		
+		/** Line with of the arcs. */
 		var lineWidth = 8;
 		
 		/** Rotation of the clock view. */
 		var phase = - Math.PI / 2;
 		
-		var canvasContext = canvas.getContext('2d');
+		/** Main canvas, take care of the ID. */
+		var frontCanvas = $("#canvas")[0];
 		
+		/** 2D context of the front canvas. */
+		var frontCtx = canvas.getContext('2d');
+		
+		// create a back canvas
+		/** Back canvas. */
+		var backCanvas = document.createElement('canvas');
+		
+		// set the size of the back canvas
+		backCanvas.width = frontCanvas.width;
+		backCanvas.height = frontCanvas.height;
+		
+		// get the back canvas' context and set the line width for drawing
+		/** 2D context of the back canvas. */
+		var backCtx = backCanvas.getContext('2d');
+		backCtx.lineWidth = lineWidth;
+		
+		/**
+		 * Set the values of the arcs in [0..1] interval based on the octal time object of the octoclock.
+		 * Value 1 means full circle.
+		 *
+		 * @param octalTime Time object of octoclock#getTime().
+		 */
 		var setTime = function(octalTime) {
 			var timeSegment = octalTime.timeNum(6);
 			arcs.outer.value = (timeSegment & 07777) / 07777;
@@ -35,24 +67,64 @@ define(["jquery", "octojs-clock"], function($, octoclock) {
 		/**
 		 * Draw the arcs.
 		 * Value of the arcs must be set before invoking this method.
-		 * 
-		 * @param canvasContext Canvas context to draw with.
 		 */
 		var draw = function() {
-			canvasContext.lineWidth = lineWidth;
 			for(var i in arcs) {
 				var arcLength = Math.PI * 2 * arcs[i].value + phase;
-				canvasContext.beginPath();
-				canvasContext.arc(canvas.width / 2, canvas.height / 2, arcs[i].radius, phase, arcLength, false);
-				canvasContext.strokeStyle = arcs[i].color;
-				canvasContext.stroke();
+				var origin = { x: backCanvas.width / 2, y:  backCanvas.height / 2 };
+				backCtx.beginPath();
+				backCtx.arc(origin.x, origin.y, arcs[i].radius, phase, arcLength, false);
+				backCtx.strokeStyle = arcs[i].color;
+				backCtx.stroke();
 			}
 		};
 		
+		/**
+		 * Draws an arrow head into a circle.
+		 *
+		 * The distance of the arrow head from the origin will be radius.
+		 * The arrow head will point at (lineAngle + phase) or -(lineAngle + phase) direction, where phase is a global variable. The sign depends on the outer parameter.
+		 *
+		 * @param context Drawing context.
+		 * @param origin Point of origin, it has x and y properties.
+		 * @param radius Distance of the tip of the arrow head from the origin.
+		 * @param lineAngle Angle of the arrow.
+		 * @param color Html color of the arrow head.
+		 * @param filled Flag to fill the arrow head.
+		 * @param outer Flag to put the arrow head outside the cicle, with the opposite direction.
+		 */
+		var drawArrow = function(context, origin, radius, lineAngle, color, filled, outer) {
+			var tip = { 
+				x: origin.x + radius * Math.cos(lineAngle + phase),
+				y: origin.y + radius * Math.sin(lineAngle + phase)
+			};
+			var sidePoints = [];
+			
+			context.beginPath();
+
+			context.strokeStyle = color;
+			context.stroke();
+		};
+		
+		/**
+		 * Public method to update TimeArcsView objects.
+		 * It sets the value of the arcs, clears the canvas and redraws the arcs.
+		 *
+		 * @param octalTime Time object of octoclock#getTime().
+		 */
 		var update = function(octalTime) {
+			// clear the back canvas
+			backCtx.clearRect(0, 0, backCanvas.width, backCanvas.height);
+			
+			// set the value of the arcs
 			setTime(octalTime);
-			canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+			
+			// draw the updated arcs into the back canvas
 			draw();
+			
+			// clear the front canvas and copy the arcs from the back canvas into the front canvas.
+			frontCtx.clearRect(0, 0, frontCanvas.width, frontCanvas.height);
+			frontCtx.drawImage(backCanvas, 0, 0);
 		};
 		
 		var timeArcsView = {};
@@ -60,61 +132,73 @@ define(["jquery", "octojs-clock"], function($, octoclock) {
 		return timeArcsView;
 	};
 	
-	/**
-	 * Draws an arrow head into a circle.
+	/** 
+	 * Textual view of the clock.
+	 * This is a constructor function.
 	 *
-	 * The distance of the arrow head from the origin will be radius.
-	 * The arrow head will point at (lineAngle + phase) or -(lineAngle + phase) direction, where phase is a global variable. The sign depends on the outer parameter.
-	 *
-	 * @param context Drawing context.
-	 * @param origin Point of origin, it has x and y properties.
-	 * @param radius Distance of the tip of the arrow head from the origin.
-	 * @param lineAngle Angle of the arrow.
-	 * @param color Html color of the arrow head.
-	 * @param filled Flag to fill the arrow head.
-	 * @param outer Flag to put the arrow head outside the cicle, with the opposite direction.
+	 * Public method: update(daysText, hoursText)
+	 * 
 	 */
-	var drawArrow = function(context, origin, radius, lineAngle, color, filled, outer) {
-		var tip = { 
-			x: origin.x + radius * Math.cos(lineAngle + phase),
-			y: origin.y + radius * Math.sin(lineAngle + phase)
-		};
-		var sidePoints = [];
+	var TextView = function() {
 		
-		context.beginPath();
+		/** The div element of this view. Take care of the ID. */
+		var textFrame = $("div.clock-frame");
+		
+		/**
+		 * Updates the view.
+		 *
+		 * @param daysText String representation of days.
+		 * @param hoursText String representation of hours.
+		 */
+		var update = function(daysText, hoursText) {
+			textFrame.hide();
+			textFrame.children("#ohours").text(hoursText);
+			textFrame.children("#odays").text(daysText);
+			textFrame.show();
+		}
+		
+		var textView = {};
+		textView.update = update;
+		return textView;
+	}
 
-		context.strokeStyle = color;
-		context.stroke();
-	};
-
+	/** Suffix of the page title. */
 	var pageTitleSuffix = " - Octoclock";
-	var canvas = $("#canvas")[0];
-	var canvasContext = canvas.getContext('2d');
-	var buffer = document.createElement('canvas');
-	buffer.width = canvas.width;
-	buffer.height = canvas.height;
-	var timeArcsView = new TimeArcsView(buffer);
+	
+	/** Visualization of the octal clock. */
+	var views = {};
 	
 	/**
 	 * Method to call on each animation frame.
 	 * Calculates the time based on octoclock and renders the views.
 	 */
 	var render = function() {
+		// get the octal time object from octoclock
 		var octalTime = octoclock.getTime();
-		var textFrame = $("div.clock-frame");
-		textFrame.hide();
-		canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-		timeArcsView.update(octalTime);
+		
+		// update the arcs view
+		views.timeArcsView.update(octalTime);
+		
+		// extract hours and days from the time object
 		var hoursText = octalTime.hours(4);
 		var daysText = octalTime.days;
-		textFrame.children("#ohours").text(hoursText);
-		textFrame.children("#odays").text(daysText);
+		
+		// update the text view
+		views.textView.update(daysText, hoursText);
+		
+		// update the title of the page
 		document.title = hoursText + ":" + daysText + pageTitleSuffix;
-		textFrame.show();
-		canvasContext.drawImage(buffer, 0, 0);
+		
+		// schedule a re-render when the browser is ready
 		requestAnimationFrame(render);
 	};
-	
-	render();
 
+	$(function() {
+		// construct the views only after the whole document is loaded
+		views.timeArcsView = new TimeArcsView();
+		views.textView = new TextView();
+		
+		// start the rendering loop
+		render();
+	});
 });
