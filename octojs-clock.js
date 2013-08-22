743234b45af57c33a06c6f27efc364c702f665b7
Peter Ungvari <ungvari.mail@gmail.com>
2013-08-22 16:34:58 +0200

    Bugfixes and refactors. drawArrow() unfinished method is added.

--- octojs-clock.js
+++ octojs-clock.js
@@ -13,22 +13,50 @@ define(function() {
 	var BEGINING_OF_TIME = Date.UTC(1960, 3, 6, 6, 0, 0, 0);
 
 	/**
-	 * Clock precision. Defines the number of octal digits.
+	 * The octal time, digits are placed from the less significant to the higher significant from left to right.
 	 */
-	var PRECISION_TIMES_A_DAY = 0100000000;
-
 	var getTime = function() {
-	    var now = Date.now();
-	    return Math.floor((timeInDays(timeInMillis(now)) * PRECISION_TIMES_A_DAY)).toString(8);
-	}
+		var millis = timeInMillis(Date.now());
+		var hours = (millis % DAY_IN_MILLIS) / DAY_IN_MILLIS;
+		var days = Math.floor(millis / DAY_IN_MILLIS);
+		var radix = 8;
+		var daysString = days.toString(radix).split("").reverse().join("");
+	    return {
+			daysNum: days,
+			days: daysString,
+			hoursNum: function(precision) {
+				return Math.floor(hours * Math.pow(radix, precision));
+			},
+			hours: function(length) {
+				return pad(this.hoursNum(length).toString(radix).split("").reverse().join(""), length);
+			},
+			timeNum: function(hoursPrecision) {
+				return this.daysNum * Math.pow(radix, hoursPrecision) + this.hoursNum(hoursPrecision);
+			},
+			time: function(length) {
+				return this.hours(length) + this.days;
+			}
+		};
+	};
 
-	var timeInDays = function(timeInMillis) {
-		return timeInMillis / DAY_IN_MILLIS;
-	}
-
+	/**
+	 * Milliseconds from the beginnig of time.
+	 */
 	var timeInMillis = function(millisSinceEpoch) {
 		return millisSinceEpoch - BEGINING_OF_TIME;
-	}
+	};
+	
+	/**
+	 * Pad a number from the left with zeroes to reach the preferred size.
+	 * Maximum 8 zeroes are supported besides the digits of the number. 
+	 *
+	 * @param num Number to be padded.
+	 * @param size Preferred size.
+	 */
+	var pad = function(num, size) {
+		var s = "00000000" + num;
+		return s.substr(s.length-size);
+	};
 
 	var octoclock = {};
 	octoclock.getTime = getTime;
--- octojs-view.js
+++ octojs-view.js
@@ -1,58 +1,120 @@
 define(["jquery", "octojs-clock"], function($, octoclock) {
 	
+	/** Time arcs view of the clock. */
+	var TimeArcsView = function(canvas) {
+		
+		var arcs = {
+			outer: { 
+				radius: 60,
+				color: 'rgb(58, 91, 122)'
+			},
+			middle: {
+				radius: 45,
+				color: 'rgb(108, 136, 164)'
+			},
+			inner: {
+				radius: 30,
+				color: 'rgb(158, 181, 206)'
+			}
+		};
+		
+		var lineWidth = 8;
+		
+		/** Rotation of the clock view. */
+		var phase = - Math.PI / 2;
+		
+		var canvasContext = canvas.getContext('2d');
+		
+		var setTime = function(octalTime) {
+			var timeSegment = octalTime.timeNum(6);
+			arcs.outer.value = (timeSegment & 07777) / 07777;
+			arcs.middle.value = (timeSegment & 0777700) / 0777700;
+			arcs.inner.value = (timeSegment & 077770000) / 077770000;
+		};
+		
+		/**
+		 * Draw the arcs.
+		 * Value of the arcs must be set before invoking this method.
+		 * 
+		 * @param canvasContext Canvas context to draw with.
+		 */
+		var draw = function() {
+			canvasContext.lineWidth = lineWidth;
+			for(var i in arcs) {
+				var arcLength = Math.PI * 2 * arcs[i].value + phase;
+				canvasContext.beginPath();
+				canvasContext.arc(canvas.width / 2, canvas.height / 2, arcs[i].radius, phase, arcLength, false);
+				canvasContext.strokeStyle = arcs[i].color;
+				canvasContext.stroke();
+			}
+		};
+		
+		var update = function(octalTime) {
+			setTime(octalTime);
+			canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+			draw();
+		};
+		
+		var timeArcsView = {};
+		timeArcsView.update = update;
+		return timeArcsView;
+	};
+	
+	/**
+	 * Draws an arrow head into a circle.
+	 *
+	 * The distance of the arrow head from the origin will be radius.
+	 * The arrow head will point at (lineAngle + phase) or -(lineAngle + phase) direction, where phase is a global variable. The sign depends on the outer parameter.
+	 *
+	 * @param context Drawing context.
+	 * @param origin Point of origin, it has x and y properties.
+	 * @param radius Distance of the tip of the arrow head from the origin.
+	 * @param lineAngle Angle of the arrow.
+	 * @param color Html color of the arrow head.
+	 * @param filled Flag to fill the arrow head.
+	 * @param outer Flag to put the arrow head outside the cicle, with the opposite direction.
+	 */
+	var drawArrow = function(context, origin, radius, lineAngle, color, filled, outer) {
+		var tip = { 
+			x: origin.x + radius * Math.cos(lineAngle + phase),
+			y: origin.y + radius * Math.sin(lineAngle + phase)
+		};
+		var sidePoints = [];
+		
+		context.beginPath();
+
+		context.strokeStyle = color;
+		context.stroke();
+	};
+
+	var pageTitleSuffix = " - Octoclock";
 	var canvas = $("#canvas")[0];
 	var canvasContext = canvas.getContext('2d');
-
 	var buffer = document.createElement('canvas');
 	buffer.width = canvas.width;
 	buffer.height = canvas.height;
-	var bufferContext = buffer.getContext('2d');
-
+	var timeArcsView = new TimeArcsView(buffer);
+	
+	/**
+	 * Method to call on each animation frame.
+	 * Calculates the time based on octoclock and renders the views.
+	 */
 	var render = function() {
+		var octalTime = octoclock.getTime();
 		var textFrame = $("div.clock-frame");
 		textFrame.hide();
 		canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-		bufferContext.clearRect(0, 0, buffer.width, buffer.height);
-		var timeString = octoclock.getTime();
-		var seconds = parseInt(timeString.substring(timeString.length-4, timeString.length), 8);
-		var minutes = parseInt(timeString.substring(timeString.length-6, timeString.length-2), 8);
-		var hours = parseInt(timeString.substring(timeString.length-8, timeString.length-4), 8);
-		var days = parseInt(timeString.substring(0, timeString.length-8), 8);
-		var hoursText = pad(hours.toString(8), 4).split("").reverse().join("");
-		var daysText = days.toString(8).split("").reverse().join("");
+		timeArcsView.update(octalTime);
+		var hoursText = octalTime.hours(4);
+		var daysText = octalTime.days;
 		textFrame.children("#ohours").text(hoursText);
 		textFrame.children("#odays").text(daysText);
-		var now = new Date();
-		textFrame.children("#chours").text(now.toTimeString());
-		textFrame.children("#cdays").text(now.toLocaleDateString());
-		textFrame.children("#cdate").text(now.toUTCString());
-		document.title = hoursText + ":" +  days.toString(8) + " - Octoclock";
+		document.title = hoursText + ":" + daysText + pageTitleSuffix;
 		textFrame.show();
-		drawArcs(bufferContext, hours, minutes, seconds);
 		canvasContext.drawImage(buffer, 0, 0);
 		requestAnimationFrame(render);
-	}
-
-	var drawArcs = function(context, h, m, s) {
-		var phase = - 3.14 / 2;
-		context.lineWidth = 8;
-		
-		var drawArc = function(r, unit, color) {
-			context.beginPath();
-			context.arc(canvas.width / 2, canvas.height / 2, r, phase, 3.14 * 2 * unit / 010000 + phase, false);
-			context.strokeStyle = color;
-			context.stroke();
-		}
-
-		drawArc(60, s, 'rgb(58, 91, 122)');
-		drawArc(45, m, 'rgb(108, 136, 164)');
-		drawArc(30, h, 'rgb(158, 181, 206)');
-	}
-
-	var pad = function(num, size) {
-		var s = "000000000" + num;
-		return s.substr(s.length-size);
-	}
-
+	};
+	
 	render();
+
 });
